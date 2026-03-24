92e6fbd8e8d73d1b626b10630cd4820d8f0640ae
Abhishek121206 <abhishek121206@gmail.com>
2026-03-24 17:11:44 +0530

    feat: add user register api

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-require('dotenv').config();
 const connectDB = require("./config/db");
 const express=require("express");
 const app=express();
+const User = require("./models/User");
 
+require('dotenv').config();
+
+//middleware
 app.use(express.json());
 
+//connect DB
+connectDB();
+
+//test route
 app.get("/",(req,res)=>{
     res.send("API Running");
-})
-app.listen(5000,()=> console.log("Server started"));
+});
+
+//register API
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.json(user);
+});
+
+//debug 
 console.log("MONGO_URI:", process.env.MONGO_URI);
-connectDB();
+
+app.listen(5000,()=> console.log("Server started"));
+
+
